cb9561aa963f8d9e8f1d846225d00cd404416c54
prajwal851851 <prajwaldhital851@gmail.com>
2025-12-20 16:38:13 +0545

    add

--- scraper/scraper/items.py
+++ scraper/scraper/items.py
@@ -1,7 +1,24 @@
-# scraper/scraper/items.py
+# File: scraper/scraper/items.py
+# COMPLETE REPLACEMENT
+
 import scrapy
 
+
+class StreamingItem(scrapy.Item):
+    """Item for movies/shows with streaming links"""
+    movie_pk = scrapy.Field()
+    imdb_id = scrapy.Field()
+    title = scrapy.Field()
+    year = scrapy.Field()
+    type = scrapy.Field()
+    poster_url = scrapy.Field()
+    synopsis = scrapy.Field()
+    original_detail_url = scrapy.Field()
+    links = scrapy.Field()  # List of dicts: [{quality, language, source_url}]
+
+
 class MovieItem(scrapy.Item):
+    """Legacy item for backward compatibility"""
     imdb_id = scrapy.Field()
     title = scrapy.Field()
     year = scrapy.Field()
